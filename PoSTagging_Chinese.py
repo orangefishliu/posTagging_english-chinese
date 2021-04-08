d0409acc0921de626022b01bb599ef28872f08a1
Ting-Yu Liu <48986216+orangefishliu@users.noreply.github.com>
2021-04-08 23:10:46 +0200

    accuracy comparison
    compare the accuracy of pos tagging between simplified characters and traditional characters

--- PoSTagging_Chinese.py
+++ PoSTagging_Chinese.py
@@ -2,8 +2,12 @@ import spacy
 from spacy import displacy
 
 nlp = spacy.load("zh_core_web_sm")
+
+# simplified Chinese
 doc = nlp("你最近怎么样？")
+# traditional Chinese
 doc1 = nlp("今天天氣很好。")
+# traditional Chinese
 doc2 = nlp("你今天下午想跟我一起出去玩嗎?")
 
 for token in doc:
@@ -17,6 +21,7 @@ print("\n")
 for token in doc2:
     print(token.text, ":", token.lemma_, token.pos_, token.tag_, token.dep_)
 
+# visualize dependency relations
 displacy.serve(doc, style="dep")
 displacy.serve(doc1, style="dep")
 displacy.serve(doc2, style="dep")
